2cc22829ffeea85e991b5ffa3254a1413e3a189e
pmatveeva <polink.matveeva@yandex.ru>
2016-09-23 14:29:15 +0300

    Add files via upload

---
+++ dict_algs.py
@@ -0,0 +1,34 @@
+ivan = {
+    "name": "ivan",
+    "age": 34,
+    "children": [{
+        "name": "vasja",
+        "age": 12,
+    },
+        {
+        "name": "petja",
+        "age": 10,
+    }
+    ],
+}
+darja = {
+    "name": "darja",
+    "age": 41,
+    "children": [{
+        "name": "kirill",
+        "age": 21,
+    },
+    {
+        "name": "pavel",
+        "age": 25 ,
+    }
+    ],
+}
+
+emps = [ivan, darja]
+
+for el in emps:
+    for el1 in el["children"]:
+        if el1['age'] > 18:
+            print(el["name"])
+            break
